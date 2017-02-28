cf0930bd58ac5d515fba7d5a55644e2fb73c9a9b
christianraymond <ombenisn.raymond@gmail.com>
2017-02-28 18:29:48 +0200

    first commit

---
+++ test/greet.test.js
@@ -0,0 +1,18 @@
+/*var testCase = require('mocha').describe;
+var assert = require('chai').assert;*/
+
+describe('The greet function', function(){
+
+    it('should greet Chris correctly', function(){
+
+        assert.equal('Hello, Chris', greet('Chris'));
+    });
+});
+
+describe('The greet function', function(){
+
+    it('should greet  Andrew correctly', function(){
+
+        assert.equal('Hello, Andrew', greet('Andrew'));
+    });
+});
